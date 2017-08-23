9e01a0b23321488358f47379608f2972deeb43e4
Dean Jenkins <skullandbones99@gmail.com>
2017-08-23 09:24:28 +0100

    mainwindow: Rework serial port slot connection
    Tidy up the serial port slot connection and disconnection code
    so that the serial port needs to be successfully open first.
    
    In the mainwindow destructor ~MainWindow, disconnect the timer and
    serial port slots. Also delete the timer and serial port objects
    plus add debug.
    
    If ~MainWindow was called without calling SerialPortDiscovery()
    then ~MainWindow would crash doing portInUse->isOpen() because
    portInUse was NULL. Therefore, add a NULL pointer check for
    portInUse.
    
    Signed-off-by: Dean Jenkins <skullandbones99@gmail.com>

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -148,9 +148,17 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete optimizer;
+    if (timer) {
+        qDebug() << "~MainWindow: Disconnecting timer";
+        disconnect(timer, SIGNAL(timeout()), this, SLOT(RxData()));
+        delete timer;
+    }
     //Close open port
-    if (portInUse->isOpen()  ) {
+    if (portInUse && portInUse->isOpen()) {
+        qDebug() << "~MainWindow: Disconnecting serial port";
+        disconnect(portInUse, SIGNAL(readyRead()), this, SLOT(readData()));
         portInUse->close();
+        delete portInUse;
     }
     delete ui;
 }
@@ -166,7 +174,6 @@ void MainWindow::SerialPortDiscovery()
     portInUse->setStopBits(QSerialPort::OneStop);
     portInUse->setFlowControl(QSerialPort::NoFlowControl);
     //10 bits????
-    connect(portInUse, SIGNAL(readyRead()), this, SLOT(readData()));
 
     //get a list of ports
     serPortInfo = QSerialPortInfo::availablePorts();
@@ -192,6 +199,7 @@ void MainWindow::OpenComPort(const QString *portName)
     qDebug() <<"MainWindow::OpenComPort";
     //Close open port
     if (portInUse->isOpen()) {
+        disconnect(portInUse, SIGNAL(readyRead()), this, SLOT(readData()));
         qDebug() << "Closing Port:" << portInUse->portName();
         portInUse->close();
     }
@@ -202,6 +210,7 @@ void MainWindow::OpenComPort(const QString *portName)
     QString msg;
     if ( portInUse->open(QIODevice::ReadWrite))
     {
+        connect(portInUse, SIGNAL(readyRead()), this, SLOT(readData()));
         msg = QString("Port %1 opened").arg(comport);
         SaveCalFile(); //Update cal file with new port info
     }
