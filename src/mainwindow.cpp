1394651ece6b6e51ca75b32de4d53964bab5acdd
Dean Jenkins <skullandbones99@gmail.com>
2018-12-23 12:54:25 +0000

    Release version 1.3a.2
    This version has Dean's modifications on top of the 1.3a.1 baseline.
    
    The improvements include:
    
    1.  Upgraded to qcustomplot v2.0.0 to take any improvements since
        the v1.3.1 version used in the original 1.3a codebase.
    
    2.  Improved the discharging of the uTracer when recovering from a
        a loss of communications and during a "stop" user action.
    
    3.  Cleaned-up CreateTestVectors() to only read the data once instead
        of multiple times so that the dataset is consistent with itself.
    
    4.  Modified the plotting of "real" Va to reduce quantisation noise
        from the ADC. Therefore, when ploting use the calculated Va value
        and not the ADC value. This means noise now only appears in the
        measured Ia axis. Consectutive plots are now more consistent
        as the Va values no longer contains any randomness from the ADC.
    
    5.  Increased the allowable slider ranges to accommodate the needs
        of my uTracer configuration.
    
    6.  Fixed a gm1Del typo in saving data.csv. The variable should of
        been gm2Del. However, the fix has no impact to compatibility of
        data.csv files.
    
    7.  For reference, added my calibration and data.csv files.
    
    8.  Added missing return values to MainWindow::CloseComPort() to
        prevent a crash of the "release" uTmax image when
        $HOME/utmax_files did not exist.
    
    9.  Improved the print out of the no uTmax_files directory error
        message so that the "release" uTmax image would report the
        error when $HOME/utmax_files did not exist.
    
    10. Improved the title of the dialogue pop-up for selecting the
        vacuum tube data file so that the needed action of the user
        is clearer to understand.
    
    Signed-off-by: Dean Jenkins <skullandbones99@gmail.com>

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -124,7 +124,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->AutoPath->setText(QDir::homePath());
     QPixmap logo = QPixmap(":/uTracer.png");
     ui->Logo->setPixmap(logo);
-    this->setWindowTitle("uTmax 1.3a.2-rc1");
+    this->setWindowTitle("uTmax 1.3a.2");
 
     ClearLCDs();
 
